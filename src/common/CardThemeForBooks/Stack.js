f410411b621b447454199138ebea213eb47ec0be
Filip_Mac <filiptankiewicz04@gmail.com>
2022-05-13 15:19:47 -0700

    timer added

--- src/common/CardThemeForBooks/Stack.js
+++ src/common/CardThemeForBooks/Stack.js
@@ -4,99 +4,91 @@ import { Stack, HStack, Heading, Text } from 'native-base';
 import { dataBooks } from '../../Books/DataBooks/data';
 import Time from '../Time/Time';
 
-export default function StackContainer() {
-
-    const start = new Date(2011, 2, 5);
-    const end   = new Date(2011, 5, 5);
-    const range = moment.range(start, end);
-
-    range.diff('months'); // 3
-    range.diff('days');   // 92
-    range.diff();         // 7945200000
-
+export default function StackContainer(props) {
     return (
+        <Stack
+            p="4"
+            space={3}
+            height={300}
+        >
             <Stack
-                p="4"
-                space={3}
+                space={2}
             >
-                <Stack
-                    space={2}
+                <Heading
+                    size="md"
+                    ml="-1"
                 >
-                    <Heading
-                        size="md"
-                        ml="-1"
-                    >
-                        {dataBooks.map((item, id) => (
-                            <Text
-                                key={`card-${id + 1}`}
-                            >
-                                {item.titlePT}
-                            </Text>
-                        ))}
-                    </Heading>
-                    <Text
-                        fontSize="xs"
-                        _light={{
-                            color: "violet.500"
-                        }}
-                        _dark={{
-                            color: "violet.400"
-                        }}
-                        fontWeight="500"
-                        ml="-1"
-                        mt="-1"
-                    >
-                        {dataBooks.map((item, author) => (
-                            <React.Fragment
-                                key={author}
-                            >
-                                <Text
-                                    _light={{
-                                        color: "#afb0b3"
-                                    }}
-                                    _dark={{
-                                        color: "gray.400"
-                                    }}
-                                >
-                                    {item.authorText} {""}
-                                </Text>
-                                {item.authorPT}
-                            </React.Fragment>
-                        ))}
-                    </Text>
-                </Stack>
-                <Text
-                    margin={0}
-                    color="black"
-                    letterSpacing={1}
-                    fontSize="sm"
-                >
-                    {dataBooks.map((item, content) => (
-                        <React.Fragment
-                            key={content}
+                    {dataBooks.map((item, id) => (
+                        <Text
+                            key={`card-${id + 1}`}
                         >
-                            {item.contentPT}
+                            {item.titlePT}
+                        </Text>
+                    ))}
+                </Heading>
+                <Text
+                    fontSize="xs"
+                    _light={{
+                        color: "violet.500"
+                    }}
+                    _dark={{
+                        color: "violet.400"
+                    }}
+                    fontWeight="500"
+                    ml="-1"
+                    mt="-1"
+                >
+                    {dataBooks.map((item, author) => (
+                        <React.Fragment
+                            key={author}
+                        >
+                            <Text
+                                _light={{
+                                    color: "#afb0b3"
+                                }}
+                                _dark={{
+                                    color: "gray.400"
+                                }}
+                            >
+                                {item.authorText} {""}
+                            </Text>
+                            {item.authorPT}
                         </React.Fragment>
                     ))}
                 </Text>
+            </Stack>
+            <Text
+                margin={0}
+                color="black"
+                letterSpacing={1}
+                fontSize="sm"
+            >
+                {dataBooks.map((item, content) => (
+                    <React.Fragment
+                        key={content}
+                    >
+                        {item.contentPT}
+                    </React.Fragment>
+                ))}
+            </Text>
+            <HStack
+                alignItems="center"
+                space={4}
+                justifyContent="flex-end"
+            >
                 <HStack
                     alignItems="center"
-                    space={4}
-                    justifyContent="flex-end"
                 >
-                    <HStack
-                        alignItems="center"
-                    >
-                        <Text
-                            color="coolGray.600"
-                            _dark={{
-                                color: "warmGray.200"
-                            }}
-                            fontWeight="400">
-                             Godzina: {diff}
-                        </Text>
-                    </HStack>
+                    <Text
+                        color="coolGray.600"
+                        _dark={{
+                            color: "warmGray.200"
+                        }}
+                        fontWeight="400">
+                        Do wakacji pozostało: <Time {...props}/> 
+                    </Text>
                 </HStack>
-            </Stack>
+            </HStack>
+        </Stack>
     );
 };
--- src/common/Time/Time.js
+++ src/common/Time/Time.js
@@ -1,25 +1,50 @@
 
 import * as React from 'react';
+import { View, Text } from 'native-base';
+
 
 export default function Time() {
 
-    const [time, setTime] = React.useState(null)
+    const [days, setDays] = React.useState(0);
+    const [hours, setHours] = React.useState(0);
+    const [minutes, setMinutes] = React.useState(0);
+    const [seconds, setSeconds] = React.useState(0);
 
     React.useEffect(() => {
-        let time = getCurrentTime();
-        setTime(time);
+        const target = new Date("05/14/2022 23:59:59");
+
+        const interval = setInterval(() => {
+            const now = new Date();
+            const difference = target.getTime() - now.getTime();
+
+            const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+            setDays(d);
+
+            const h = Math.floor(
+                (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+            );
+            setHours(h);
+
+            const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+            setMinutes(m);
+
+            const s = Math.floor((difference % (1000 * 60)) / 1000);
+            setSeconds(s);
+        }, 1000);
+
+        return () => clearInterval(interval);
     }, []);
-
-    const getCurrentTime = () => {
-        let today = new Date();
-        let hours = (today.getHours() < 10 ? '0' : '') + today.getHours();
-        let minutes = (today.getMinutes() < 10 ? '0' : '') + today.getMinutes();
-        return hours + ':' + minutes;
-    }
-
     return (
-        <>
-            {time}
-        </>
-    );
+        <View
+            flexDirection="row"
+        >
+            <Text
+                marginRight={2}
+            >{days}</Text>
+            <Text
+            marginRight={2}>{hours}</Text>
+            <Text marginRight={2}>{minutes}</Text>
+            <Text marginRight={2}>{seconds}</Text>
+        </View>
+    )
 };
